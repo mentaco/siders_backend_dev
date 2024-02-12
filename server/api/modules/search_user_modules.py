ecbbc44ac39eb939a98863c31e3b204d962b4bac
hikapyon <hikaru.okafuji@gmail.com>
2024-02-12 15:47:53 +0900

    add search_user_modules

---
+++ server/api/modules/search_user_modules.py
@@ -0,0 +1,21 @@
+from flask import jsonify, request
+from .execute_query import exec_query
+
+# データを取得する関数の例
+def search_user_get(search_text):
+    try:
+        params = ('%' + search_text + '%', '%' + search_text + '%')
+        query = """
+        SELECT student_id, handle_name, header_image
+        FROM student_info_t
+        WHERE student_id LIKE %s
+        OR handle_name LIKE %s;
+        """
+        result = exec_query(query, params)
+        return jsonify({'data': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
