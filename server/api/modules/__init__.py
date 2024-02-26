abbb6927fa1ee4e99c09aeadedc4afa4b82a19b6
96knoha <kei.h0220@icloud.com>
2024-02-26 11:02:12 +0900

    space addition

--- server/api/modules/__init__.py
+++ server/api/modules/__init__.py
@@ -3,4 +3,4 @@ from .follow import *
 from .message_modules import *
 from .notice_modules import *
 from .get_follow_info import *
-from.get_follower_info import *
+from .get_follower_info import *
