af3bd2b7ed7597f2eb2ff988c160a441cfda5221
KeiyaNimura <asxx1375@gmail.com>
2024-04-18 23:32:13 +0900

    投稿のいいね数を返す関数を更新

--- server/api/modules/get_like_count.py
+++ server/api/modules/get_like_count.py
@@ -3,18 +3,13 @@ from .execute_query import exec_query
 
 #投稿のいいね数を取得する関数
 def get_like_count(post_id):
+    post_id = int(post_id)
     try:
-        query = """
-        SELECT COUNT(*)
-        FROM liked_post_t
-        WHERE post_id = %s;
-        """
         params = (post_id,)
-        result = exec_query(query, params)
-        if result:
-            return jsonify(result)
-        else:
-            return jsonify({'error': 'Post not found'}), 404
+        query = 'SELECT COUNT(*) FROM liked_post_t WHERE post_id = 1'
+        result = exec_query(query, params, fetch_all=True)
+        return jsonify({'data':result[0]['count']})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
