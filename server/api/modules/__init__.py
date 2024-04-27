74a013e8b8da7c8531eac5c07a0aed918611e19a
KeiyaNimura <asxx1375@gmail.com>
2024-04-28 00:06:51 +0900

    コメントを取得するAPIを作成

--- server/api/modules/__init__.py
+++ server/api/modules/__init__.py
@@ -17,3 +17,4 @@ from .get_bookmark_status import *
 from .get_like_status import *
 from .post_liked_user_info import *
 from .post_bookmarked_user_info import *
+from .get_timeline_comment import *
